19627f18ca177e0c6a17ff0ec4f1c4a539641701
Daniel Haß <daniel@hass.onl>
2020-08-20 12:33:50 +0000

    annotated extractor interface

--- continuum_deployer/extractors/extractor.py
+++ continuum_deployer/extractors/extractor.py
@@ -1,6 +1,7 @@
 from continuum_deployer.deployment import Deployment
 
 class Extractor:
+    """Interface that defines interaction with several DSL specific extractor implementations."""
 
     _app_modules = []
 
@@ -8,11 +9,14 @@ class Extractor:
         pass
 
     def parse(self, dsl_input):
+        """Handles actual parsing of DSL to internal data structures. Needs to be implemented by child."""
         raise NotImplementedError
 
     def get_app_modules(self):
+        """Getter method for application modules"""
         return self._app_modules
 
     def print_app_modules(self):
+        """Convenience helper that prints object representation of application modules"""
         for module in self._app_modules:
             print(module)
