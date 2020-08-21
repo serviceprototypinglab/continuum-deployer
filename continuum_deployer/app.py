ae30786aeb08ca56976ed32b0a5d411cffeae2d0
Daniel Haß <daniel@hass.onl>
2020-08-21 08:29:44 +0000

    added click for cli interaction

--- continuum_deployer/app.py
+++ continuum_deployer/app.py
@@ -1,15 +1,27 @@
+import click
+
 from continuum_deployer.extractors.helm import Helm
 
 
-def main():
+@click.group()
+def cli():
+    pass
 
-    stream = open('./charts/redis/redis.yaml', 'r')
 
-    helm = Helm()
-    helm.parse(stream)
-    helm.print_app_modules()
+@cli.command()
+@click.option('-f', '--file', required=True, help='Path to DSL file')
+@click.option('-t', '--type', type=click.Choice(['helm']), default='helm')
+def print_resources(file, type):
+
+    stream = open(file, 'r')
+
+    if type == 'helm':
+        helm = Helm()
+        helm.parse(stream)
+        helm.print_app_modules()
+    else:
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
-    # execute only if run as a script
-    main()
+    cli()
