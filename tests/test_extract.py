978bd8328c9b0c1fdcf81533e7d4a1bd6920dc1d
Daniel Haß <daniel@hass.onl>
2020-08-21 07:58:24 +0000

    added tests + fixtures

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,14 +1,30 @@
 import pytest
 from continuum_deployer.extractors.helm import Helm
 
-def test_resource_extract():
+
+@pytest.fixture(scope="function")
+def extractor():
+    return Helm()
+
+
+def test_resource_extract(extractor):
     stream = open('./tests/yaml/resources.yaml', 'r')
 
-    helm = Helm()
-    helm.parse(stream)
-    modules = helm.get_app_modules()
+    extractor.parse(stream)
+    modules = extractor.get_app_modules()
 
     assert modules[0].resources_requests == \
         {"cpu": "100m", "memory": "256Mi"}
     assert modules[0].resources_limits == \
         {"cpu": "200m", "memory": "512Mi"}
+
+
+def test_multi_component_extract(extractor):
+    stream = open('./tests/yaml/multi_component.yaml', 'r')
+
+    print(extractor.print_app_modules())
+
+    extractor.parse(stream)
+    modules = extractor.get_app_modules()
+
+    assert len(modules) == 2
