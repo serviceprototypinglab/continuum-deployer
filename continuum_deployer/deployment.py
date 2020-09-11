740f02e9abe106882108d95f7db9db4ead3c2610
Daniel Hass <hassdan1@students.zhaw.ch>
2020-09-11 09:12:34 +0000

    switched defaults for numberic values to 0

--- continuum_deployer/deployment.py
+++ continuum_deployer/deployment.py
@@ -8,16 +8,16 @@ class DeploymentEntity:
     # name of the deployment
     name: str = field(default=None)
     # amount of memory the deployment requires to run
-    memory: float = field(default=None)
+    memory: float = field(default=0)
     # amount of memory the deployment is allowed to use at max
     # TODO - not implemented
-    memory_limit: float = field(default=None)
+    memory_limit: float = field(default=0)
     # amount of cpu resources the deployment requires to run
     # 1=1 cpu core | 0.5=0.5 cpu core
-    cpu: float = field(default=None)
+    cpu: float = field(default=0)
     # number of cpu cores the deployment is allowed to use at max
     # TODO - no implemented
-    cpu_limit: float = field(default=None)
+    cpu_limit: float = field(default=0)
     # raw extracted yaml definition
     yaml: dict = field(default=None)
     # assigned labels
