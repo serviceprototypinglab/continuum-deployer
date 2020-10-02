1cc7f6fa054f9f174c403f9e3e3ee3702855a552
Daniel Hass <hassdan1@students.zhaw.ch>
2020-10-02 08:58:50 +0000

    removed hard-coded modules path

--- continuum_deployer/utils/plugin_loader.py
+++ continuum_deployer/utils/plugin_loader.py
@@ -1,22 +1,31 @@
-from continuum_deployer.utils.plugin_manager import CDPluginManager
+import os
 
+from continuum_deployer.utils.plugin_manager import CDPluginManager
 from continuum_deployer.solving.solver import Solver
 
 
 class PluginLoader:
 
-    PLUGIN_PATH = '/workspaces/continuum-deployer/continuum_deployer/plugins'
-
     def __init__(self):
+
+        self.plugins_paths = []
+
+        # add default plugins path
+        _cp_module_path = os.path.realpath(__file__).split('/utils')
+        self.add_plugins_path('{}/plugins'.format(_cp_module_path[0]))
+
         # Build the manager
         self.plugin_manager = CDPluginManager()
         # Tell it the default place(s) where to find plugins
-        self.plugin_manager.setPluginPlaces([self.PLUGIN_PATH])
+        self.plugin_manager.setPluginPlaces(self.plugins_paths)
         # Define the various categories corresponding to the different
         # kinds of plugins you have defined
         self.plugin_manager.setCategoriesFilter({
             "Solver": Solver,
         })
+
+    def add_plugins_path(self, path):
+        self.plugins_paths.append(path)
 
     def get_plugin_manager(self):
         return self.plugin_manager
