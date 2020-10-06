615cc979ce7fcd8ae86d2bea962f072dec845b3b
Daniel Hass <hassdan1@students.zhaw.ch>
2020-10-06 08:39:01 +0000

    removed non-interactive match

--- continuum_deployer/app.py
+++ continuum_deployer/app.py
@@ -59,53 +59,6 @@ def print_resources(file):
 
 
 @cli.command()
-@click.option('-r', '--resources', required=True, help=_HELPTEXT_RESOURCES)
-@click.option('-d', '--deployment', required=True, help=_HELPTEXT_DSL)
-@click.option('-t', '--type', type=click.Choice(Importer.DSL_TYPES), default='helm', show_default=True, help=_HELPTEXT_TYPE)
-@click.option('-s', '--solver', type=click.Choice(['sat', 'greedy']), default='sat', show_default=True, help=_HELPTEXT_SOLVER)
-@click.option('-o', '--output', 'output_path', type=str, help=_HELPTEXT_OUTPUT)
-def nonint_match(resources, deployment, type, solver, output_path):
-    """
-        Match deployments non-interactively using cli parameters
-    """
-
-    _deployment_file = open(deployment, 'r')
-
-    deployment_entities = None
-    if type == 'helm':
-        helm = Helm()
-        helm.parse(_deployment_file)
-        deployment_entities = helm.get_app_modules()
-    else:
-        raise NotImplementedError
-
-    _resources_file = open(resources, 'r')
-    resources = Resources()
-    resources.parse(_resources_file)
-
-    if solver == 'sat':
-        matcher = SAT(deployment_entities, resources.get_resources())
-    elif solver == 'greedy':
-        matcher = Greedy(deployment_entities, resources.get_resources())
-    matcher.match()
-    matcher.print_resources()
-
-    resources_matched = matcher.get_resources()
-
-    file = None
-
-    if output_path is None:
-        exporter = Exporter(stdout=True)
-    else:
-        file = open(output_path, "w")
-        exporter = Exporter(output_stream=file)
-    exporter.export(resources_matched)
-
-    if file is not None:
-        file.close()
-
-
-@cli.command()
 @click.option('-r', '--resources', required=False, default=None, help=_HELPTEXT_RESOURCES)
 @click.option('-d', '--deployment', required=False, default=None, help=_HELPTEXT_DSL)
 @click.option('-t', '--type', type=click.Choice(Importer.DSL_TYPES), default=None, show_default=True, help=_HELPTEXT_TYPE)
